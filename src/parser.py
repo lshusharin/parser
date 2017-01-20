12a855bab2b49a3512239591947ce97c387caddc
leonidshusharin <leonid.shusharin@tstechpro.com>
2017-01-20 20:05:39 +0200

    last

--- src/parser.py
+++ src/parser.py
@@ -14,11 +14,10 @@ class Parser(object):
 
     def parse_file(self):
         print type(self.text)
-        ids = re.findall(r'\b\d{3} ', self.text)
-        print len(ids)
-        vals = re.findall(r'=\d+', self.text)
-        print len(vals)
+
+        vals = re.findall(r'(\b\d{3}) \S+=(\d+\b)', self.text)
+        print vals
         packed = {}
-        for i, j in enumerate(ids):
-            packed[j] = vals[i].replace('=', '')
-        print packed
+        # for i, j in enumerate(ids):
+        #     packed[j] = vals[i].replace('=', '')
+        # print packed
