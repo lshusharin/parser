dea2d480a88f45ce44e2ff7d8bd1ca3c21a1f7e9
leonidshusharin <leonid.shusharin@tstechpro.com>
2017-01-23 11:21:37 +0200

    Json-packing method added

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 import sys
 from src.parser import Parser
 
-
+#run this file from your console, previously switched to the project root
+# and set the path to the log-file, which you want to parse
+#Sample: "python main.py log.log"
 if __name__ == "__main__":
     file_r = sys.argv[1]
 
